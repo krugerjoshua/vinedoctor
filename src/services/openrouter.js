2fa84910e489c9fd11c5ba27aed1bc50301007bd
kruger.joshua <109550490+krugerjoshua@users.noreply.github.com>
2026-08-19 15:39:57 +0200

    Update openrouter.js
    Updated HTTP-Referer to deployed url

--- src/services/openrouter.js
+++ src/services/openrouter.js
@@ -6,7 +6,7 @@ export async function diagnoseImage(base64Image, mimeType) {
     headers: {
       "Authorization": `Bearer ${apiKey}`,
       "Content-Type": "application/json",
-      "HTTP-Referer": "http://localhost:5173",
+      "HTTP-Referer": "https://vinedoctor.netlify.app/",
       "X-Title": "VineDoctor",
     },
     body: JSON.stringify({
